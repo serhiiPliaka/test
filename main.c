d3aee67d3c6eb292742ea450c06c26057cadac4e
serhiiPliaka <151433256+serhiiPliaka@users.noreply.github.com>
2023-11-19 11:36:36 +0200

    Create main.c
    создан первый файл

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "main.h"
+
+
+int main(int argc, char *argv[]) {
+
+
+printf("count = %d\n", count);
+printf("cos(x)=0;  x = %lf\n", result_x);
+
+
+return 0;
